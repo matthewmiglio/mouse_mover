a37ac984de1015d132947257401a8cd2a1aa9429
matthew-work-PC <matthew.miglio@magna.com>
2024-10-16 16:20:52 -0400

    user input

--- mouse_mover.py
+++ mouse_mover.py
@@ -1,5 +1,5 @@
-import pyautogui
 import random
+import pyautogui
 import time
 
 def format_time(t):
@@ -12,10 +12,25 @@ def format_time(t):
 
     return f'{hours}H {minutes}M {seconds}S'
 
-
+def get_hours_input():
+    input_query_string = '\n\nEnter a time in hours to move mouse for: '
+    while 1:
+        inp = input(input_query_string)
+        try:
+            inp=float(inp)
+            if inp > 24:
+                print('Cannot work for more than 24 hours!')
+                continue
+            if inp < 0.00001:
+                print('Cannot work for negative hours!')
+                continue
+            
+            return  inp
+        except:
+            print('Hour input must be castable to a float!')
 
 # Set the duration to run the script (in seconds)
-run_hours = 2
+run_hours = get_hours_input()
 run_duration = run_hours * 60 * 60  
 start_time = time.time()
 
@@ -31,9 +46,9 @@ while time.time() - start_time < run_duration:
     y = random.randint(0, screen_height - 1)
 
     # Move the mouse to the random coordinates
-    pyautogui.moveTo(x, y, duration=3)  # Smooth movement over 0.5 seconds
+    pyautogui.moveTo(x, y, duration=0.1)  # Smooth movement over 0.5 seconds
 
     # Wait for 10 seconds before the next move
-    time.sleep(6)
+    time.sleep(random.randint(20,30))
 
 print("Finished moving the mouse.")
