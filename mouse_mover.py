0d106f1d10cb90b1e795f3e97a9c6f52d2f24d07
matthew-work-PC <matthew.miglio@magna.com>
2024-11-27 10:53:39 -0500

    smaller movements | if name == main wrapper

--- mouse_mover.py
+++ mouse_mover.py
@@ -32,17 +32,33 @@ def get_hours_input():
             print("Hour input must be castable to a float!")
 
 
-run_hours = get_hours_input()
-run_duration = run_hours * 60 * 60
-start_time = time.time()
+def calc_new_pos(screen_width, screen_height):
+    current_pos = pyautogui.position()
+    current_x, current_y = current_pos
+    new_x = current_x + random.randint(-2, 2)
+    new_y = current_y + random.randint(-2, 2)
 
-while time.time() - start_time < run_duration:
-    time_left = start_time + run_duration - time.time()
-    print(f"time left: {format_time(time_left)}")
+    if new_x < 0 or new_x > screen_width or new_y < 0 or new_y > screen_height:
+        return calc_new_pos(screen_width, screen_height)
+
+    return new_x, new_y
+
+
+def main():
+
+    run_hours = get_hours_input()
+    run_duration = run_hours * 60 * 60
+    start_time = time.time()
     screen_width, screen_height = pyautogui.size()
-    x = random.randint(0, screen_width - 1)
-    y = random.randint(0, screen_height - 1)
-    pyautogui.moveTo(x, y, duration=0.2)
-    time.sleep(random.randint(20, 30))
 
-print("Finished moving the mouse.")
+    while time.time() - start_time < run_duration:
+        time_left = start_time + run_duration - time.time()
+        print(f"time left: {format_time(time_left)}")
+        pyautogui.moveTo(*calc_new_pos(screen_width, screen_height), duration=0.2)
+        time.sleep(random.randint(50, 60))
+
+    print("Finished moving the mouse.")
+
+
+if __name__ == "__main__":
+    main()
